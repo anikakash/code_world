e79f37483a721513569ca2c6a1818ee8201b43f1
anikakash <anik15-14210@diu.edu.bd>
2020-07-08 23:01:50 +0600

    code template

---
+++ Tamplate-1.cpp
@@ -0,0 +1,16 @@
+//thanks God For Every Thing!
+//contest link:
+#include<bits/stdc++.h>
+#define pi                      acose(-1)
+
+typedef long long int           ll;
+typedef double                  dl;
+using namespace std;
+
+
+
+int main()
+{
+
+    return 0;
+}
