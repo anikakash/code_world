f9e958ad5fd4546b33ebb52612654e434f1d4c11
anikaksh <anikdash989@gmail.com>
2020-10-12 16:14:02 +0600

    edit

--- Tamplate-1.cpp
+++ Tamplate-1.cpp
@@ -1,12 +1,19 @@
 //thanks God For Every Thing!
 //contest link:
-#include<bits/stdc++.h>
-#define pi                      acose(-1)
+//#include<bits/stdc++.h>
+#include<iostream>
+#include<string>
+#include<algorithm>
+#include<cstdio>
+#define pi           acose(-1)
+#define flush        cin.ignore(numeric_limits<streamsize>::max(),'\n');
+#define wow          ios_base::sync_with_stdio(0); cin.tie(0); cout.tie(0);
+#define max          170000
+
+using namespace std;
 
 typedef long long int           ll;
 typedef double                  dl;
-using namespace std;
-
 
 
 int main()
